8a6e538d59c1b2199d89e95d8bf55e646b626ac1
XiaotongZhao <275451183@qq.com>
2021-08-08 09:09:55 +0800

    add unitTest projecct

---
+++ AlgorithmServiceUnitTest/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace AlgorithmServiceUnitTest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
