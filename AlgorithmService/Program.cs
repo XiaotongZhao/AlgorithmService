25550f16bf09d0c9a6abd1f72329f7256d4f051c
XiaotongZhao <275451183@qq.com>
2025-12-22 15:47:47 +0800

    引入elk

--- AlgorithmService/Program.cs
+++ AlgorithmService/Program.cs
@@ -1,9 +1,12 @@
+using Elastic.Serilog.Sinks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
-
+ConfigureLogging();
+builder.Host.UseSerilog();
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -38,10 +41,61 @@ static void implementDIByScanLibrary(IServiceCollection services)
 
     foreach (var interfaceClass in interfacesAndImplementClasses)
     {
-        interfaceClass.Interfaces.ForEach(Interface => 
+        interfaceClass.Interfaces.ForEach(Interface =>
         {
             services.AddScoped(Interface, interfaceClass.ImplementInterfaceClass);
         });
     }
 
+}
+
+
+void ConfigureLogging()
+{
+    var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+    var configuration = new ConfigurationBuilder()
+        .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+        .AddJsonFile(
+            $"appsettings.{environment}.json",
+            optional: true)
+        .Build();
+
+    var elasticAddress = configuration["ElasticConfiguration:Uri"];
+    var userName = configuration["ElasticConfiguration:UserName"] ?? string.Empty;
+    var password = configuration["ElasticConfiguration:PassWord"] ?? string.Empty;
+    if (!string.IsNullOrEmpty(elasticAddress) && !string.IsNullOrEmpty(environment))
+        Log.Logger = new LoggerConfiguration()
+            .Enrich.FromLogContext()
+            .WriteTo.Console()
+            .WriteTo.Elasticsearch(ConfigureElasticSink(elasticAddress, userName, password, environment))
+            .Enrich.WithProperty("Environment", environment)
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
+    else
+        Log.Logger = new LoggerConfiguration()
+          .Enrich.FromLogContext()
+          .WriteTo.Console()
+          .Enrich.WithProperty("Environment", environment)
+          .ReadFrom.Configuration(configuration)
+          .CreateLogger();
+}
+
+Serilog.Sinks.Elasticsearch.ElasticsearchSinkOptions ConfigureElasticSink(string elasticAddress, string userName, string password, string environment)
+{
+    var index = $"{Assembly.GetExecutingAssembly()?.GetName()?.Name?.ToLower()}-{environment?.ToLower()}-{DateTime.UtcNow:yyyy-MM}";
+    return new Serilog.Sinks.Elasticsearch.ElasticsearchSinkOptions(new Uri(elasticAddress))
+    {
+        AutoRegisterTemplate = true,
+        IndexFormat = index,
+        ModifyConnectionSettings = connection =>
+        {
+            if (!string.IsNullOrEmpty(userName) && !string.IsNullOrEmpty(password))
+                connection.BasicAuthentication(userName, password);
+            else
+                connection
+                .ServerCertificateValidationCallback((o, certificate, chain, errors) => true)
+                .DisableAutomaticProxyDetection();
+            return connection;
+        }
+    };
 }
